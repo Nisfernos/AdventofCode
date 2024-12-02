b70f646f65b2ff1b835db1e087b96ab257cd771f
Ashe van Leijsen <ashe.van.leijsen@technolution.nl>
2024-12-02 08:43:13 +0100

    Template

---
+++ 2024/Days/template.py
@@ -0,0 +1,15 @@
+from Utils.Day import DayFormat
+
+
+class Day(DayFormat):
+
+    def __init__(self) -> None:
+        super().__init__(2024, 0)
+
+    def part_one(self):
+        pass
+
+    def part_two(self):
+        pass
+
+Day()
---
+++ Utils/Day.py
@@ -0,0 +1,19 @@
+class DayFormat():
+
+    def __init__(self, year: int, day: int) -> None:
+        day = f"{day:02d}"
+        self.input = self.parse_input(f"{year}\\Input\\day{day}.txt")
+        self.test_input = self.parse_input(f"{year}\\Input\\test.txt")
+        self.part_one()
+        self.part_two()
+
+    def parse_input(self, text_file) -> None:
+        with open(text_file) as txt:
+            return txt.readlines()
+
+    def part_one(self):
+        pass
+
+    def part_two(self):
+        pass
+        
